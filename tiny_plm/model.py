fd6a83cf6b5862620a30bd448a15f6db1da128fb
nimuh <nima@dino.dev>
2024-06-26 18:46:35 -0700

    adjusted attention module to be for self attn

--- tiny_plm/model.py
+++ tiny_plm/model.py
@@ -1,25 +1,48 @@
-
 from config import PLMConfig
 from torch import nn
+import torch.nn.functional as F
+import math
 
-# TODO
+
 class SelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
+        assert config.n_embed % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embed, 3 * config.n_embed)
+        self.c_proj = nn.Linear(config.n_embed, config.n_embed)
+        self.n_head = config.n_head
+        self.n_embed = config.n_embed
+
+    def forward(self, x):
+        B, T, C = x.size()
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embed, dim=2)
+
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
 
 
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.c_fc = nn.Linear(config.n_embed, 4*config.n_embed)
-        self.gelu = nn.GELU(approximate='tanh')
-        self.c_proj = nn.Linear(4*config.n_embed, config.n_embed)
+        self.c_fc = nn.Linear(config.n_embed, 4 * config.n_embed)
+        self.gelu = nn.GELU(approximate="tanh")
+        self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
 
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
         return x
+
 
 class Block(nn.Module):
     def __init__(self, config):
@@ -34,16 +57,19 @@ class Block(nn.Module):
         x = x + self.mlp(self.ln_2(x))
         return x
 
+
 class PLM(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.config = config
 
-        self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embed),
-            wpe = nn.Embedding(config.block_size, config.n_embed),
-            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
-            ln_f = nn.LayerNorm(config.n_embed),
-        ))
+        self.transformer = nn.ModuleDict(
+            dict(
+                wte=nn.Embedding(config.vocab_size, config.n_embed),
+                wpe=nn.Embedding(config.block_size, config.n_embed),
+                h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+                ln_f=nn.LayerNorm(config.n_embed),
+            )
+        )
 
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
