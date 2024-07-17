2edbc71390b97a0e02cdde2aebe3ae50b3682053
nimuh <nima@dino.dev>
2024-07-17 11:21:03 -0700

    added missing parts for training to just run

--- scripts/run.py
+++ scripts/run.py
@@ -8,38 +8,42 @@ import torch
 # TODO
 # add cmd args to this script
 
-DATA = '../data/prok.csv'
+DATA = '../data/all_peptides.csv'
+epochs = 500
 
-df_seqs = pd.read_csv(DATA, nrows=10)
+df_seqs = pd.read_csv(DATA, nrows=100)
 batch_idxs = protein_batch_create(df_seqs.shape[0])
 
-config = PLMConfig(n_head=8, n_layer=1, vocab_size=22)
+config = PLMConfig(n_head=8, n_layer=2, vocab_size=22)
 model = PLM(config=config)
 
 tokenizer = ProteinTokenizer()
 padded_value = 21
+opt = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
+for epoch in range(epochs):
+    for batch in batch_idxs:
+        seqs = df_seqs.iloc[batch].sequence
+        max_size = max([len(seq) for seq in seqs])
+        tok_seqs = []
+        labels = []
 
-for batch in batch_idxs:
-    seqs = df_seqs.iloc[batch].sequence
-    max_size = max([len(seq) for seq in seqs])
-    tok_seqs = []
-    labels = []
+        for seq in seqs:
+            tok_masked, label = tokenizer.encode(seq)
+            tok_seqs.append(tok_masked)
+            labels.append(label)
 
-    for seq in seqs:
-        tok_masked, label = tokenizer.encode(seq)
-        tok_seqs.append(tok_masked)
-        labels.append(label)
+        padded = [torch.cat([item, torch.tensor([padded_value]).expand(max_size-len(item))]) for item in tok_seqs]
+        batch_padded = torch.cat([item[None] for item in padded])
 
-    padded = [torch.cat([item, torch.tensor([padded_value]).expand(max_size-len(item))]) for item in tok_seqs]
-    batch_padded = torch.cat([item[None] for item in padded])
+        padded_label = [torch.cat([item, torch.tensor([padded_value]).expand(max_size-len(item))]) for item in labels]
+        batch_padded_label = torch.cat([item[None] for item in padded_label])
 
-    padded_label = [torch.cat([item, torch.tensor([padded_value]).expand(max_size-len(item))]) for item in labels]
-    batch_padded_label = torch.cat([item[None] for item in padded_label])
-
-    y = model(batch_padded)
-    pred = y.view(y.size(0)*y.size(1), -1)
-    targets = batch_padded_label.view(batch_padded_label.size(0)*batch_padded_label.size(1))
-    loss = torch.nn.functional.cross_entropy(pred, targets, ignore_index=21)
-    print(torch.exp(loss))
-
+        y = model(batch_padded)
+        pred = y.view(y.size(0)*y.size(1), -1)
+        targets = batch_padded_label.view(batch_padded_label.size(0)*batch_padded_label.size(1))
+        loss = torch.nn.functional.cross_entropy(pred, targets, ignore_index=21)
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+        print(f"Perplexity: {torch.exp(loss)}  CE Loss: {loss}")
